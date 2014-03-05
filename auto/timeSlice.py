dc9ff29a20cf6cf4bed7f092465a101ca51c9fbe
jjardel <johnjardel@gmail.com>
2014-03-05 17:41:25 -0600

    market-segmented time slice of the data

--- auto/timeSlice.py
+++ auto/timeSlice.py
@@ -52,40 +52,39 @@ with open( 'market_segs.out' ) as fp:
 marketNames = { 0: 'Midwest', 1: 'West', 2: 'Southwest',
                 3: 'Northeast', 4: 'Southeast' }
 
+colors = [ 'red', 'purple', 'blue', 'yellow', 'green' ]
 
 for market in marketDict:
     X = df.loc[ marketDict[ market ], : ].values
     means = np.mean( X, axis = 0 )
     norm = means[ 0 ]
     means /= norm
-    plt.plot( x, means, label = marketNames[ market ] )
+    plt.plot( x, means, label = marketNames[ market ], color = colors[ market ] )
 
 plt.ylim( [ 0., 1.2 ] )
+plt.ylabel( 'Sales (normalized)' )
+plt.xlabel( 'Month' )
+bad = c( 10467, 7437, 9894, 10728, 15948 )
+data = read.csv( 'market_segs.out' )
+names( data ) <- c( "dealerID", "lat", "lon", "Market" )
+data[ data$dealerID %in% bad, 2:3 ] = NA
+dealers = data
+
+dealers$Market[ dealers$Market == 0 ] = "Midwest"
+dealers$Market[ dealers$Market == 1 ] = "West"
+dealers$Market[ dealers$Market == 2 ] = "Southwest"
+dealers$Market[ dealers$Market == 3 ] = "Northeast"
+dealers$Market[ dealers$Market == 4 ] = "Southeast"
+
+baseMap <- get_map( location = c( lon = -96.78, 38.37 ),
+                        maptype="hybrid", source="google", zoom = 4)
+
+map = ggmap( baseMap ) + geom_point( aes( x = lon, y = lat,
+  color = factor( Market ) ),  data = dealers ) + xlab( "" ) + ylab("")x
+
+ggsave( map, file = "map.png" )
+
 
 plt.legend( loc = 'lower right' )
-plt.show()
-
-"""means = np.mean( X, axis = 0 )
-plt.plot( x, means, 'k' )
-plt.plot( x, df.loc[ 9774, : ], 'r' )
-
-
-"""
-
-"""
-for dealer in dealerIDs:
-    y = df.loc[ dealer, : ]
-    y /= np.mean( y )
-    plt.plot( x, y )
-"""    
-
-#plt.show()
-    
-    
-
-
-
-
-#plt.plot( month, volume )
-#plt.show()
+plt.savefig( 'market_time.png' )
 
