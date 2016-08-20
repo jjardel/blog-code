93064297ae66be5c8ab28245665f67690c176ac7
jjardel <jjardel@square-root.com>
2016-08-20 13:22:42 -0500

    extracting tweet info using Twitter API

--- trump-insults/extract/extract_tweets.py
+++ trump-insults/extract/extract_tweets.py
@@ -5,9 +5,11 @@ import argparse
 from src import TweetExtractor
 
 
-def main():
+def main(path):
 
     twex = TweetExtractor()
+    twex.extract()
+    twex.export_to_csv(path)
 
 
 if __name__ == '__main__':
@@ -15,10 +17,9 @@ if __name__ == '__main__':
     root_logger = get_root_logger()
     get_header(root_logger, "Extracting all of Trump's insulting Tweets. Lucky Me...")
 
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('--path', required=True, help='where would you like to extract to?')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', required=True, help='where would you like to extract to?')
 
-    #args = parser.parse_args()
+    args = parser.parse_args()
 
-    #main(args.path)
-    main()
+    main(args.path)
--- trump-insults/extract/src/_tweet_extractor.py
+++ trump-insults/extract/src/_tweet_extractor.py
@@ -4,7 +4,8 @@ from util import get_path
 # third party modules
 import tweepy
 
-import pandas as pd
+from pandas import read_csv
+from pandas.io.json import json_normalize
 import json
 
 
@@ -23,18 +24,51 @@ class TweetExtractor(object):
 
         auth = tweepy.OAuthHandler(config['consumer_token'], config['consumer_secret'])
         auth.set_access_token(config['access_token'], config['access_secret'])
+        self.logger.info('Successfully Authenticated to Twitter API')
 
         self.api = tweepy.API(auth)
 
-    def load(self):
+    def extract(self):
 
-        pass
+        try:
+            insults_df = read_csv(self.loc.format('data/insults.csv'))
+        except FileNotFoundError:
+            self.logger.critical('Insults file not found.  extract_insults.py must be run first')
 
+        # get the schema of a Tweet
+        tweets_df = json_normalize(self.api.get_status(insults_df.loc[0, 'tweet_id'])._json)
+        tweets_df.drop(0, inplace=True)  # delete the data
 
+        # build a DF of tweet data for each tweet in insults data
+        # iterate in chunks so we can use twitter's GET statuses endpoint for bulk search
 
+        n_insults = len(insults_df)
+        chunksize = 100
 
+        cursor = 0
+        while cursor < n_insults:
+
+            cursor_end_pos = min([cursor + chunksize - 1, n_insults - 1])
+            self.logger.debug('Loading tweets {0}-{1}'.format(cursor, cursor_end_pos))
+
+            tweet_ids = insults_df.ix[cursor: cursor_end_pos, 'tweet_id'].tolist()
+            res = self.api.statuses_lookup(tweet_ids)
+            for item in res:
+                tweet = json_normalize(item._json)
+                tweets_df = tweets_df.append(tweet)
+
+            cursor += chunksize
+
+        self.tweets_df = tweets_df
+
+    def export_to_csv(self, path):
+
+        self.logger.info('Writing results to {0}'.format(path))
+        self.tweets_df.to_csv(path)
 
 
 if __name__ == '__main__':
 
+    # for testing
     twex = TweetExtractor()
+    twex.extract()
